71a4b0fbe3746cff0300e969cf21362531ea1013
Gene Gotimer <otherdevopsgene@portinfo.com>
2024-05-16 21:01:42 -0400

    chore: update versions for devops class

--- 06-webserver/modules/webserver/providers.tf
+++ 06-webserver/modules/webserver/providers.tf
@@ -2,9 +2,9 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.25.0"
+      version = ">= 5.49.0"
     }
   }
 
-  required_version = ">= 1.6.2"
+  required_version = ">= 1.8.3"
 }
